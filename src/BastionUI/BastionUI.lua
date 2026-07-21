59f9e832b03ba5c1f2bd53ef4becb93567e3d83d
tcxcore <tcxcore@hotmail.com>
2026-07-22 02:07:37 +0800

    feat: 更新面板上的版本号

--- src/BastionUI/BastionUI.lua
+++ src/BastionUI/BastionUI.lua
@@ -871,7 +871,7 @@ function BastionUI:CreateMainFrame()
     -- 顶部 Header 标题
     local title = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
     title:SetPoint("TOPLEFT", 24, -14)
-    title:SetText("BASTION v1.0.6")
+    title:SetText("BASTION v1.0.7")
     title:SetTextColor(0.18, 0.65, 1.0)
 
     -- 顶部 976px 蓝紫发光分隔线
